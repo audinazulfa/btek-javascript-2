23bb1b9b5bc71d4b112e737e36c146a9c5049a9b
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-10 19:07:07 +0700

    kurang while fix

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ rline.question('Berapa Jarak Tempuh? : ', function(jarak) {
 
     if(jarak <= 2) {
       const harga = 8000;
-      console.log("Total Ongkos Kirim : " + harga)
+      console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(harga))
      
       const layanan = 0.045 * (harga)
-      console.log("Biaya Layanan : " + layanan)
+      console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(layanan))
     }
     else {
       while(jarak >= 2) {
@@ -20,10 +20,10 @@ rline.question('Berapa Jarak Tempuh? : ', function(jarak) {
         const harga = 8000
 
         const harga2 = harga + (tarif*(jarak-2))
-        console.log("Total Ongkos Kirim : " + harga2)
+        console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(harga2))
 
         const layanan = 0.045 * harga2
-        console.log("Biaya Layanan : " + layanan)
+        console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(layanan))
         jarak++;
         break;
       }
