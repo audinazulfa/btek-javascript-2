e21a5859580de6cb21f067daec0f859c63a7f907
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-15 10:23:21 +0700

    promise

--- index.js
+++ index.js
@@ -1,63 +1,53 @@
+const { rejects } = require('assert')
+const { resolve } = require('path')
 const readline = require('readline')
 const rline = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
+const error = () => {
+    rline.close()
+}
 
-const mulai = async() => {
-  let running = true;
+const proses = () => {
+    return new Promise((resolve, reject) => {
+        rline.question('Berapa Jarak Tempuh Anda ? : ', function(jarak) {
+            console.log(`${jarak}`);
 
-  while(running) {
-    try{
-      var recursiveAsyncReadLine = function () {
-        rline.question('Berapa Jarak Tempuh? : ', function(jarak) {
-          console.log(`${jarak}`);
-          
-          rline.question('Apakah ingin diulang? ', function(recursiveAsyncReadLine) {
-            console.log(`${recursiveAsyncReadLine}`);
-          if(jarak <= 2) {
-            const harga = 8000;
-            console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(harga))
-           
-            const layanan = 0.045 * (harga)
-            console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(layanan))
-          }
-          else {
-            while(jarak >= 2) {
-              const tarif = 5000
-              const harga = 8000
-      
-              const harga2 = harga + (tarif*(jarak-2))
-          
-              const layanan = 0.045 * harga2
-              
-              jarak++;
-            }
-            console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(harga2))
-            console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(layanan))
-          }
-          recursiveAsyncReadLine();
-          rline.question('Apakah ingin diulang? ', function(recursiveAsyncReadLine) {
-            if (answer.toLowerCase() == 'n') {
-              rline.close()
+            if(jarak <= 2) {
+                const harga = 8000
+                console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(harga))
+
+                const layanan = 0.045 * harga
+                console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(layanan))
             } else {
-              question()
-            }
-        });
-          });
-        });
-      };
-    } catch(error) {
-      console.log("Thankyouu");
-      running = false;
+                const tarif = 5000
+                    const harga = 8000
+                    
+                    const harga2 = harga + (tarif * (jarak - 2))
+                    const layanan = 0.045 * harga2
+                    console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(harga2))
+                    console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR"}).format(layanan))
+            }    
+            console.log("\n")
+            resolve(true)
+        })  
+    })
+}
+
+const mulai = async() =>{
+    let running = true
+    while(running) {
+        try {
+            const data = await proses();
+            running = data
+        }
+        catch(error) {
+            console.log("Thankyou!")
+            running = false;
+        }
     }
-  }
 }
 
 mulai();
-
-rline.on('close', function() {
-  console.log('\nBye!');
-  process.exit(0);
-})
